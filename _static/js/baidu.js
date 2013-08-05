3a688bd4d03cbec53cc89490ff78d6c86f147ba4
Z. Liu <zhixu.liu@gmail.com>
2013-08-05 11:52:03 +0800

    commit html

---
+++ _static/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?684b70c2a8043a97e5e806fa3c58fe18";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+})();
